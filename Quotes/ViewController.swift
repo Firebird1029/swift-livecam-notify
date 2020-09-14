d6b520ec42e475535f2b6a3dbdc89937594a0377
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-09-13 16:31:52 -1000

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Brandon Yee on 9/13/20.
+//  Copyright © 2020 Brandon Yee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
